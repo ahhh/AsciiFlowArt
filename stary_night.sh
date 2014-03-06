c43403d30e136ac1389230ee59bdc03a3b8c5f7e
ahhh <ahhh.db@gmail.com>
2014-03-05 23:25:21 -0800

    Update stary_night.sh

--- stary_night.sh
+++ stary_night.sh
@@ -1,2 +1,2 @@
 #/bin/bash 
-cat /dev/urandom | xxd -b | cut -c 10-60 | sed 's/\(.*\)/\1\1/g' | sed 's/ //g' | sed "s/1/,/g" | sed 's/0/./g' | sed 's/,,/✦/g' | sed 's/\.\./\☽/g' | sed 's/\☽\☽\☽/ݠ?/g' | sed 's/\☽\☽/\@/g' | cut -c 10-40 | sed '/,/ s/$/./' | sed '/,/ s/^/./'/'
+cat /dev/urandom | xxd -b | cut -c 10-60 | sed 's/\(.*\)/\1\1/g' | sed 's/ //g' | sed "s/1/,/g" | sed 's/0/./g' | sed 's/,,/✦/g' | sed 's/\.\./\☽/g' | sed 's/\☽\☽\☽/ݠ?/g' | sed 's/\☽\☽/\@/g' | cut -c 10-40 | sed '/,/ s/$/./' | sed '/,/ s/^/./'
